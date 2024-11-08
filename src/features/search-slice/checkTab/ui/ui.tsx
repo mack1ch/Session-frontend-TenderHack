f617ef3bdec972e8508a35998151cc89950fc0ee
mack1ch <dimastepanov.1995@mail.ru>
2024-11-09 02:46:10 +0300

    changed: checkTab, sessionsUnPublishFilters

--- src/features/search-slice/checkTab/ui/ui.tsx
+++ src/features/search-slice/checkTab/ui/ui.tsx
@@ -1,11 +1,25 @@
 import { CheckInput } from "@/entities/search-slice/checkInput";
 import styles from "./ui.module.scss";
+import { Empty } from "antd";
 
 export const CheckTab = () => {
   return (
     <>
       <section className={styles.checkTabWrap}>
         <CheckInput />
+        <div className={styles.contentWrap}>
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "center",
+            }}
+          >
+            <Empty description="Введите ссылку на сессию"></Empty>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/features/search-slice/sessionsUnPublishFilters/ui/ui.tsx
+++ src/features/search-slice/sessionsUnPublishFilters/ui/ui.tsx
@@ -1,4 +1,4 @@
-import { Checkbox } from "antd";
+import { Checkbox, Tooltip } from "antd";
 import styles from "./ui.module.scss";
 
 export const SessionsUnPublishFilters = () => {
@@ -7,17 +7,23 @@ export const SessionsUnPublishFilters = () => {
       <section className={styles.filtersLayout}>
         <h2 className={styles.h2}>Фильтры для проверки</h2>
         <div className={styles.filters}>
-          <Checkbox>Проверка на дублирующие сессии</Checkbox>
+          <Tooltip title>
+            <Checkbox>Проверка на дублирующие сессии</Checkbox>
+          </Tooltip>
           <Checkbox>Проверка наличия требования обеспечения заявок </Checkbox>
           <Checkbox>Проверка соответствия спецификации с Проектом КС</Checkbox>
           <Checkbox>Проверка карточек оферт на набор товаров</Checkbox>
           <Checkbox>
             Проверка правильности введенных данных Заказчика (ИНН, КПП и тп){" "}
           </Checkbox>
-          <Checkbox>
-            Проверка Заказчика по 44-ФЗ (не должен иметь задолжностей, не должен
-            являться иноагентом, не должен принадлежать к офшорным компаниям)
-          </Checkbox>
+          <Tooltip
+            placement="bottomRight"
+            title="(не должен иметь задолжностей, не
+              должен являться иноагентом, не должен принадлежать к офшорным
+              компаниям)"
+          >
+            <Checkbox>Проверка Заказчика по 44-ФЗ</Checkbox>
+          </Tooltip>
         </div>
       </section>
     </>
