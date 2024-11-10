37408ba3804c88f367588d7b14d8b739e46c0415
mack1ch <dimastepanov.1995@mail.ru>
2024-11-10 13:54:00 +0300

    changed: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ThemeLayout } from "@/shared/layout/themeLayout";
 import StoreProvider from "./ StoreProvider";
 import { Suspense } from "react";
+import Script from "next/script";
+import Image from "next/image";
 
 const RF_Dewi = localFont({
   src: [
@@ -39,6 +41,35 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${RF_Dewi.className}`}>
+        <Script
+          type="text/javascript"
+          id="metrika-counter"
+          strategy="afterInteractive"
+        >
+          {`     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(98874174, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+`}
+        </Script>
+        <noscript>
+          {
+            <div>
+              <Image
+                src="https://mc.yandex.ru/watch/98874174"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          }
+        </noscript>
         <AntdRegistry>
           <ThemeLayout>
             <StoreProvider>
